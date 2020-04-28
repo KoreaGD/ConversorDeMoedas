7786b0e917ee5cc223ee545c8544796cca40862e
Everton Trajano <evertontrajoli@gmail.com>
2020-04-28 20:54:45 -0300

    Updates

---
+++ precache-manifest.9b4c8a4673568644f1cdce002b60c035.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be59d5b6f858ead6ffcbbea33058c120",
+    "url": "/index.html"
+  },
+  {
+    "revision": "091e08b5ff4dcd9bfbed",
+    "url": "/static/css/main.df0f8b95.chunk.css"
+  },
+  {
+    "revision": "306eecdbedbf08b71c30",
+    "url": "/static/js/2.eaefe454.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eaefe454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091e08b5ff4dcd9bfbed",
+    "url": "/static/js/main.085a6db0.chunk.js"
+  },
+  {
+    "revision": "c7f802fa099f6d5dc882",
+    "url": "/static/js/runtime-main.6c04b9fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
