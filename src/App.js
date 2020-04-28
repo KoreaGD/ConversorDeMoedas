141a952dc01840e04c0bcb0a1dc4d50d28a00d50
Everton Trajano <evertontrajoli@gmail.com>
2020-04-28 20:40:51 -0300

    initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Conversor from './components/Conversor'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="titulo">
+        <img src="logo192.png" alt="Made with React" />
+        <p id="title">CONVERSOR REACT.JS</p>
+      </h1>
+      <div className="convertFromTo">
+        <Conversor moedaA="USD" moedaB="BRL" />
+        <Conversor moedaA="BRL" moedaB="USD" />
+      </div>
+      <div className="convertFromTo">
+        <Conversor moedaA="EUR" moedaB="BRL" />
+        <Conversor moedaA="BRL" moedaB="EUR" />
+      </div>
+      <div className="convertFromTo">
+        <Conversor moedaA="GBP" moedaB="BRL" />
+        <Conversor moedaA="BRL" moedaB="GBP" />
+      </div>
+      <div className="credits">Desenvolvido por Everton Trajano.</div>
     </div>
   );
 }
